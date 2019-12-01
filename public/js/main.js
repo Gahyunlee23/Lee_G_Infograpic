f379a0b08257bf74a543e3784894b951162fa2c0
Stella <feelis1119@naver.com>
2019-11-30 23:12:49 -0500

    fixing!

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,44 @@
 (() => {
+    
     let seeButtons = document.querySelectorAll('.coffee');
 
-    function popSeeRecipe() {
-        console.log('working!');
+    let waypoint = new Waypoint({
+        element: document.querySelector('#coffee1'),
+        handler: function(direction) {
+          console.log('Scrolled to waypoint!');
+          this.element.innerHTML += "<p>I got added with Waypoint!</p>";
 
+          let svg = this.element.firstElementChild.contentDocument;
+          // debugger;
+
+        },
+
+        offset: 300
+      });
+
+    function showRecipe(recipe, el) {
+        seeRecipe.querySelector(".recipe-legend").textContent = `Recipe: ${recipe.legend}`;
+
+        seeRecipe.classList.add('show-popover');
+
+        el.appendChild(seeRecipe);
     }
 
+    function fetchData() {
+        let url = `/info/${this.dataset.target}`,
+            targetElement = this;
 
-    seeButtons.forEach(button => button.addeventListener("click", popSeeRecipe));
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            showPopover(data, targetElement);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+
+    seeButtons.forEach(button => button.addeventListener("click", fetchData));
+    
 })();
